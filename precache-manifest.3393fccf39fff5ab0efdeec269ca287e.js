1cfaaa98801ac4d41d58bc09f586280f42436a09
Pavel Petrovich <gaara33377@gmail.com>
2019-07-09 00:23:03 +0300

    Updates

---
+++ precache-manifest.3393fccf39fff5ab0efdeec269ca287e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250b32dc5164f5c65ec17307a6e3a5ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e518244061238d0ce8d",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a1f01b7483c1d92bda36",
+    "url": "./static/css/main.985abf53.chunk.css"
+  },
+  {
+    "revision": "6e518244061238d0ce8d",
+    "url": "./static/js/2.82903f98.chunk.js"
+  },
+  {
+    "revision": "a1f01b7483c1d92bda36",
+    "url": "./static/js/main.f87956df.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
