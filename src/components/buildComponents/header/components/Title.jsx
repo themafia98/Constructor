d1aae4b487aa12f8829b954e9a926e176b172cb0
Pavel Petrovich <gaara33377@gmail.com>
2019-07-14 22:30:55 +0300

    styled-components

--- src/components/buildComponents/header/components/Title.jsx
+++ src/components/buildComponents/header/components/Title.jsx
@@ -1,12 +1,21 @@
-import React, {useState} from 'react';
-import './title.scss';
+import React, {Fragment, useState} from 'react';
+import styled from 'styled-components/macro';
+
+const Red = styled.h1`
+    font-size: ${props => props.size};
+    color: ${props => props.color};
+    text-align: center;
+`;
+
 
 const Title = props => {
     const [titleName] = useState(props.title);
     const [className] = useState(props.className);
 
     return (
-        <h1 className = {className}>{titleName}</h1>
+        <Fragment>
+            <Red color = 'red' size = '100px' >{titleName}</Red>
+        </Fragment>
     )
 }
 
