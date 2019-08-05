5dc24d26acad09ad44074b35d7b7c646064e67ce
Pavel Petrovich <gaara33377@gmail.com>
2019-08-06 02:35:32 +0300

    fix path

--- src/components/loading/Loader.jsx
+++ src/components/loading/Loader.jsx
@@ -1,8 +1,9 @@
 import React,{useState} from 'react';
 import './Loader.scss';
 export default function(props) {
-    const [path] = useState(props.path ? props.path : `${process.env.PUBLIC_URL}/img/loading.gif`);
-    const [mode] = useState(props.mode ? props.mode : false);
+  
+    const [path] = useState(props.path ? process.env.PUBLIC_URL + props.path : process.env.PUBLIC_URL + `/img/loading.gif`);
+    const [mode] = useState(props.mode ? props.mode : false);  console.log(path);
     return (
             <div className = {`loaderBox ${mode && 'specialMode'}`}>
                 <img className = 'loader' src = {path} alt = 'loader'></img> 
