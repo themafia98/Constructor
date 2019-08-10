6e295d50a0ba425cb1d33323b2cc05d6ce8ba07a
Pavel Petrovich <gaara33377@gmail.com>
2019-08-10 21:31:35 +0300

    Updates

---
+++ precache-manifest.254a5c35a241e729f931ed86048f4c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c1496eb8129768dd1e4f70e31e288a",
+    "url": "/constructor/index.html"
+  },
+  {
+    "revision": "4deb43074a2cf1cc7d8c",
+    "url": "/constructor/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5e2e32deea371cc2015f",
+    "url": "/constructor/static/css/main.43caba0e.chunk.css"
+  },
+  {
+    "revision": "4deb43074a2cf1cc7d8c",
+    "url": "/constructor/static/js/2.889c0a60.chunk.js"
+  },
+  {
+    "revision": "5e2e32deea371cc2015f",
+    "url": "/constructor/static/js/main.a2b32100.chunk.js"
+  },
+  {
+    "revision": "f81e1f478c988d0c8fd7",
+    "url": "/constructor/static/js/runtime~main.5764ba56.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/constructor/precache-manifest.c26f6395b4733dc2b7effeb41aefa95b.js"
+  "/constructor/precache-manifest.254a5c35a241e729f931ed86048f4c42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/constructor/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
